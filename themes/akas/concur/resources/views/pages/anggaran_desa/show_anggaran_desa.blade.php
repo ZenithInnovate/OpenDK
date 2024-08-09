3e8906ae3f8934ad94fb66a82b6457d79185dc08
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-08-10 00:28:58 +0800

    ok

--- themes/akas/concur/resources/views/pages/anggaran_desa/show_anggaran_desa.blade.php
+++ themes/akas/concur/resources/views/pages/anggaran_desa/show_anggaran_desa.blade.php
@@ -1,82 +1,108 @@
 @extends('layouts.app')
+@section('title', $page_description)
+
+@push('styles')
+    <style type="text/css">
+        .box-header {
+            padding: 10px;
+            margin-bottom: -5px;
+            border-radius: 5px;
+        }
+
+        .form-horizontal .form-group {
+            margin-bottom: -5px;
+        }
+
+        .form-horizontal .form-group label {
+            padding-top: 5px;
+        }
+    </style>
+@endpush
+
 @section('content')
-    <!-- Main content -->
-    <div class="col-md-8">
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                <h3 class="box-title text-bold"><i class="fa  fa-arrow-circle-right fa-lg text-blue"></i> Persentase Anggaran Desa
-                </h3>
-            </div>
-            <div class="box box-header">
-                <form class="form-horizontal">
-                    <div class="col-md-4 col-lg-4 col-sm-12">
-                        <div class="form-group">
-                            <label for="list_desa" class="col-sm-4 control-label">Desa</label>
-                            <div class="col-sm-8">
-                                <input type="hidden" id="profil_id" value="{{ $profil->id }}">
-                                <select class="form-control" id="list_desa">
-                                    <option value="Semua">Semua Desa</option>
-                                    @foreach ($list_desa as $desa)
-                                        <option value="{{ $desa->desa_id }}">{{ $desa->nama }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
+
+@include('layouts.breadcrumbs', ['title' => $page_description])
+
+<div class="section-post">
+    <div class="container">
+        <div class="row">
+            <!-- page content -->
+            <div class="col-md-8">
+                <div class="page-content">
+                    <!-- ======= Blog Single Section ======= -->
+                    <div class="dinamic-single">
+                        <div class="title">{{ 'Presentase ' . $page_title }}</div>
+                        <div class="box-header">
+                            <form class="form-horizontal">
+                                <div class="col-md-4 col-lg-4 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="list_desa" class="col-sm-4 control-label">Desa</label>
+                                        <div class="col-sm-8">
+                                            <input type="hidden" id="profil_id" value="{{ $profil->id }}">
+                                            <select class="form-control" id="list_desa">
+                                                <option value="Semua">Semua Desa</option>
+                                                @foreach ($list_desa as $desa)
+                                                <option value="{{ $desa->desa_id }}">{{ $desa->nama }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-lg-4 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="bulan" class="col-sm-4 control-label">Bulan</label>
+                                        <div class="col-sm-8">
+                                            <select class="form-control" id="list_months" name="m">
+                                                <option value="Semua">Semua</option>
+                                                @foreach (months_list() as $key => $month)
+                                                <option value="{{ $key }}">{{ $month }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-lg-4 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="list_year" class="col-sm-4 control-label">Tahun</label>
+                                        <div class="col-sm-8">
+                                            <select class="form-control" id="list_year">
+                                                <option value="Semua">Semua</option>
+                                                @foreach ($year_list as $year)
+                                                <option value="{{ $year }}">{{ $year }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </div>
-                    <div class="col-md-4 col-lg-4 col-sm-12">
-                        <div class="form-group">
-                            <label for="bulan" class="col-sm-4 control-label">Bulan</label>
-                            <div class="col-sm-8">
-                                <select class="form-control" id="list_months" name="m">
-                                    <option value="Semua">Semua</option>
-                                    @foreach (months_list() as $key => $month)
-                                        <option value="{{ $key }}">{{ $month }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
+                        <hr>
+                        <div class="box box-widget">
+                            Disini
                         </div>
-                    </div>
-                    <div class="col-md-4 col-lg-4 col-sm-12">
-                        <div class="form-group">
-                            <label for="list_year" class="col-sm-4 control-label">Tahun</label>
-                            <div class="col-sm-8">
-                                <select class="form-control" id="list_year">
-                                    <option value="Semua">Semua</option>
-                                    @foreach ($year_list as $year)
-                                        <option value="{{ $year }}">{{ $year }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="box box-widget">
-                <div class="box-header">
-                    {{-- <h3 class="box-title">Persentase Anggaran Desa</h3> --}}
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                        </button>
-                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
-                        </button>
                     </div>
                 </div>
-                <div class="box-body">
-                    <div id="chartdiv" style="width: 100%; height: 400px; overflow: hidden; text-align: left;">
-                    </div>
-                    <!-- /.row -->
+
+                <div class="page-content" id="detail_anggaran">
                 </div>
-                <!-- ./box-body -->
             </div>
-            <div id="detail_anggaran">
-            </div>
+            <!-- End page content -->
+
+            {{-- Widget --}}
+            @include('layouts.widget')
         </div>
     </div>
-    <!-- /.content -->
-    @endsection @include('partials.asset_amcharts') @include('partials.asset_select2') @push('scripts')
-    <script>
-        $(function() {
+</div>
+@endsection
 
+@include('partials.asset_amcharts')
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('.select2').select2();
+        });
+
+        $(function() {
             // Select 2 Kecamatan
             $('#list_desa').select2();
             $('#list_months').select2();
@@ -88,11 +114,11 @@
             var year = $('#list_year').find(":selected").val();
 
             /*
-             Initial Chart Dashboard Pendidikan
+             * Initial Chart Dashboard Pendidikan
              */
             das_chart_anggaran(mid, did, year);
             /*
-             End Initial
+             * End Initial
              */
 
 
@@ -136,83 +162,6 @@
 
 
         function create_chart_anggaran(data) {
-
-            /**
-             * Create the chart
-             */
-            /*AmCharts.addInitHandler( function ( chart ) {
-
-                //method to handle removing/adding columns when the marker is toggled
-                function handleCustomMarkerToggle(legendEvent) {
-                    var dataProvider = legendEvent.chart.dataProvider;
-                    var itemIndex; //store the location of the removed item
-
-                    //Set a custom flag so that the dataUpdated event doesn't fire infinitely, in case you have
-                    //a dataUpdated event of your own
-                    legendEvent.chart.toggleLegend = true;
-                    // The following toggles the markers on and off.
-                    // The only way to "hide" a column and reserved space on the axis is to remove it
-                    // completely from the dataProvider. You'll want to use the hidden flag as a means
-                    // to store/retrieve the object as needed and then sort it back to its original location
-                    // on the chart using the dataIdx property in the init handler
-                    if (undefined !== legendEvent.dataItem.hidden && legendEvent.dataItem.hidden) {
-                        legendEvent.dataItem.hidden = false;
-                        dataProvider.push(legendEvent.dataItem.storedObj);
-                        legendEvent.dataItem.storedObj = undefined;
-                        //re-sort the array by dataIdx so it comes back in the right order.
-                        dataProvider.sort(function(lhs, rhs) {
-                            return lhs.dataIdx - rhs.dataIdx;
-                        });
-                    } else {
-                        // toggle the marker off
-                        legendEvent.dataItem.hidden = true;
-                        //get the index of the data item from the data provider, using the
-                        //dataIdx property.
-                        for (var i = 0; i < dataProvider.length; ++i) {
-                            if (dataProvider[i].dataIdx === legendEvent.dataItem.dataIdx) {
-                                itemIndex = i;
-                                break;
-                            }
-                        }
-                        //store the object into the dataItem
-                        legendEvent.dataItem.storedObj = dataProvider[itemIndex];
-                        //remove it
-                        dataProvider.splice(itemIndex, 1);
-                    }
-                    legendEvent.chart.validateData(); //redraw the chart
-                }
-
-                //check if legend is enabled and custom generateFromData property
-                //is set before running
-                if (!chart.legend || !chart.legend.enabled || !chart.legend.generateFromData) {
-                    return;
-                }
-
-                var categoryField = chart.categoryField;
-                var colorField = chart.graphs.lineColorField || chart.graphs.fillColorsField || chart.graphs.colorField;
-                var legendData =  chart.dataProvider.map(function(data, idx) {
-                    var markerData = {
-                        "title": data[categoryField] + ":as " + data[chart.graphs.valueField],
-                        "color": data[colorField],
-                        "marginRight":20,
-                        "autoMargins":false,
-                        "dataIdx": idx //store a copy of the index of where this appears in the dataProvider array for ease of removal/re-insertion
-                    };
-                    if (!markerData.color) {
-                        markerData.color = chart.graphs.lineColor;
-                    }
-                    data.dataIdx = idx; //also store it in the dataProvider object itself
-                    return markerData;
-                });
-
-                chart.legend.data = legendData;
-
-                //make the markers toggleable
-                chart.legend.switchable = true;
-                chart.legend.addListener("clickMarker", handleCustomMarkerToggle);
-
-            }, ['pie'] );*/
-
             var chart = AmCharts.makeChart("chartdiv", {
                 "hideCredits": true,
                 "type": "pie",
